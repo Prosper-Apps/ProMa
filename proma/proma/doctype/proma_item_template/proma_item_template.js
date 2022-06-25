83a3816e509f1d49b984cd68ab3c514e21204ef1
Christopher Njogu <chris@chrisnjogu.com>
2022-06-25 13:04:13 +0000

    proma item template doctype

---
+++ proma/proma/doctype/proma_item_template/proma_item_template.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, phamos GmbH and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Proma Item Template', {
+	// refresh: function(frm) {
+
+	// }
+});
